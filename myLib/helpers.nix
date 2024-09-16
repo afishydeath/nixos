c103450c838a239d5550da61305eb220020d3d53
syn <afishydeath@gmail.com>
2024-09-17 01:40:35 +1000

    fixed filesIn to stop nested lists

--- myLib/helpers.nix
+++ myLib/helpers.nix
@@ -3,11 +3,12 @@ rec {
   # recursively get all files in a directory
   filesIn = path: assert (builtins.isPath path); let
     type = builtins.readFileType path;
+    deNest = list: builtins.filter (item: !(builtins.isList item)) list ++ builtins.concatLists (builtins.filter builtins.isList list);
   in
-    if type == "directory"
-      then map (nextpath: filesIn ./${path}/${nextpath}) (builtins.attrNames (builtins.readDir path))
+    if (type == "directory")
+    then deNest (map (next: filesIn (path+("/"+next))) (builtins.attrNames (builtins.readDir path)))
     else if type == "regular"
-      then path
+    then path
     else throw "Invalid input path.";
 
 }
