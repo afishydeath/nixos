9594a069da8a023da028b9969879249d02db0698
syn <afishydeath@gmail.com>
2024-10-19 22:30:08 +1100

    fix minor issues with jeezy config

--- modules/home/tui/jeezyvim/default.nix
+++ modules/home/tui/jeezyvim/default.nix
@@ -1,11 +1,35 @@
-{ config, lib, pkgs, ...}:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   options = {
     home.tui.jeezyvim.enable = lib.mkEnableOption "enable jeezyvim";
   };
   config = lib.mkIf config.home.tui.jeezyvim.enable {
     home.packages = with pkgs; [
-      jeezyvim
+      (jeezyvim.nixvimExtend {
+        config = {
+          keymaps = [
+            {
+              mode = "n";
+              action = "o";
+              key = "o";
+            }
+            {
+              mode = "n";
+              action = "<cmd>w<CR>";
+              key = "<leader>w";
+            }
+            {
+              mode = "n";
+              action = "<cmd>q<CR>";
+              key = "<leader>q";
+            }
+          ];
+        };
+      })
     ];
   };
 }
